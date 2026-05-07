daef8468cd22a9bbf21c509676891d3cc590615d
mms237-ua <mms237@alu.ua.es>
2026-05-07 09:16:34 +0200

    Logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom'
 import logo from '../imagenes/logo.png'
 
 export default function Header() {
   return (
     <header className="header">
-      <img src={logo} alt="SKALA Logo" className="header-logo" />
+      <Link to="/">
+        <img src={logo} alt="SKALA Logo" className="header-logo" />
+      </Link>
     </header>
   );
 }
